cfcdb7f9f3ecb00149f00be7203b4ad52b45535c
Mammad <thisismammad7@gmail.com>
2024-06-29 22:24:41 +0330

    create headder and half of main. responsive

---
+++ js/home.js
@@ -0,0 +1,71 @@
+let flag = true;
+window.addEventListener("resize", () => {
+  closeSideMenu();
+});
+
+function closeSideMenu() {
+  $("aside").css({
+    right: "-100%",
+  });
+  $(".menu-btn>span:nth-of-type(1)").css({
+    transform: "rotate(0)",
+    top: "5px",
+  });
+  $(".menu-btn>span:nth-of-type(2)").fadeIn(0);
+  $(".menu-btn>span:nth-of-type(3)").css({
+    transform: "rotate(0)",
+    top: "19px",
+  });
+  flag = true;
+}
+
+function openSideMenu() {
+  $("aside").css({
+    right: "0",
+  });
+  $(".menu-btn>span:nth-of-type(1)").css({
+    transform: "rotate(45deg)",
+    top: "calc(50% - 2px)",
+  });
+  $(".menu-btn>span:nth-of-type(2)").fadeOut(0);
+  $(".menu-btn>span:nth-of-type(3)").css({
+    transform: "rotate(-45deg)",
+    top: "calc(50% - 2px)",
+  });
+  flag = false;
+}
+
+function showHideMenu() {
+  flag = true;
+  $(".menu-btn").on("click", function () {
+    if (flag) {
+      openSideMenu();
+    } else {
+      closeSideMenu();
+    }
+  });
+}
+
+showHideMenu();
+
+let newFlag = true;
+$("header .nav-container .head-nav li").on("click", function () {
+  $(this).find("ul").toggle(300);
+});
+
+let newFlag1 = true;
+let id;
+$("aside .menu-nav-container .menu-nav li").on("click", function () {
+ 
+
+  $(this).find("ul").toggle(300);
+
+});
+
+$("main").on("click", function () {
+  $(".sub-menu").slideUp(300);
+  newFlag = true;
+  closeSideMenu();
+});
+
+$("a").attr("href","#");
