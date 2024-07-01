42b69c6e900ccf6a0e9bea586a06113c8f034f2e
Mammad <thisismammad7@gmail.com>
2024-07-01 19:41:03 +0330

    fixed some margins and all social media links

--- js/home.js
+++ js/home.js
@@ -106,4 +106,93 @@ $("main").on("click", function () {
   closeSideMenu();
 });
 
-$("a").attr("href", "#");
+// $("a").attr("href", "#");
+
+
+
+
+let owl1 = $("#owl-product1").owlCarousel({
+  loop: true,
+  rtl: true,
+  margin: 0,
+  dots: false,
+  nav: false,
+  center: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 4,
+    },
+  },
+});
+
+owl1.on("mousewheel", ".owl-stage", function (e) {
+  if (e.deltaY > 0) {
+    owl1.trigger("next.owl");
+  } else {
+    owl1.trigger("prev.owl");
+  }
+  e.preventDefault();
+});
+
+let owl2 = $("#owl-product2").owlCarousel({
+  loop: true,
+  rtl: true,
+  margin: 0,
+  dots: false,
+  nav: false,
+  center: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 4,
+    },
+  },
+});
+owl2.on("mousewheel", ".owl-stage", function (e) {
+  if (e.deltaY > 0) {
+    owl2.trigger("next.owl");
+  } else {
+    owl2.trigger("prev.owl");
+  }
+  e.preventDefault();
+});
+
+
+let owl3 = $("#owl-product3").owlCarousel({
+  loop: true,
+  rtl: true,
+  margin: 0,
+  dots: false,
+  nav: false,
+  center: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 4,
+    },
+  },
+});
+owl3.on("mousewheel", ".owl-stage", function (e) {
+  if (e.deltaY > 0) {
+    owl3.trigger("next.owl");
+  } else {
+    owl3.trigger("prev.owl");
+  }
+  e.preventDefault();
+});
